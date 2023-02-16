4ee3bda0942a14fb5be8205fa3eededc1a83790b
Charu Garg <Charu.Garg@Geminisolutions.com>
2023-02-16 16:54:14 +0530

    updated policy code

--- src/main/java/com/qa/mis/locators/PolicyLocator.java
+++ src/main/java/com/qa/mis/locators/PolicyLocator.java
@@ -7,4 +7,8 @@ public class PolicyLocator {
     public static By viewPolicyButton=By.xpath("//span[contains(text(),'View Policies')]");
     public static By policyTabTitle=By.xpath("//div[@class='card-block']//h5[text()='View Policies']");
     public static By policyTableData=By.xpath("//table[@id='tblActivePolicy']/tbody//td[1]");
+    public static By defaultNoOfRecordsPolicy=By.xpath("//select[@name='tblActivePolicy_length']//option[1]");
+    public static By NoOfPolicyRecords=By.xpath("//select[@name='tblActivePolicy_length']//option[2]");
+    public static By policyMessage=By.xpath("//td[@class='dataTables_empty']");
+    public static By searchPolicy=By.xpath("//input[@type='search']");
 }
--- src/main/java/com/qa/mis/stepdefinition/PolicySteps.java
+++ src/main/java/com/qa/mis/stepdefinition/PolicySteps.java
@@ -5,6 +5,7 @@ import com.gemini.generic.reporting.STATUS;
 import com.gemini.generic.ui.utils.DriverAction;
 import com.qa.mis.locators.LoginLocator;
 import com.qa.mis.locators.PolicyLocator;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebElement;
@@ -54,4 +55,58 @@ public class PolicySteps {
             GemTestReporter.addTestStep("Title", "User not able to verify the Title: " + actualMsg, STATUS.FAIL, DriverAction.takeSnapShot());
         }
     }
+    @Then("Verify number of records displayed by default")
+    public void verifyNumberOfRecordsDisplayedByDefault() {
+        String actualRecords = DriverAction.getElement(PolicyLocator.defaultNoOfRecordsPolicy).getText();
+        try {
+            if (actualRecords.equals("10"))
+                GemTestReporter.addTestStep("NumberOfRecords", "NumberOfRecords: " + actualRecords, STATUS.PASS, DriverAction.takeSnapShot());
+        } catch (Exception exception) {
+            GemTestReporter.addTestStep("NumberOfRecords", "NumberOfRecords: " + actualRecords, STATUS.FAIL, DriverAction.takeSnapShot());
+        }
+    }
+
+    @And("Select {string} as number of entries")
+    public void selectAsNumberOfEntries(String expectedRecords) {
+        DriverAction.click(PolicyLocator.NoOfPolicyRecords);
+        String actualNoOfRecords = DriverAction.getElement(PolicyLocator.NoOfPolicyRecords).getText();
+        try {
+            if (actualNoOfRecords.equals(expectedRecords))
+                GemTestReporter.addTestStep("NumberOfRecords Selected", "NumberOfRecords Selected: " + actualNoOfRecords, STATUS.PASS, DriverAction.takeSnapShot());
+        } catch (Exception exception) {
+            GemTestReporter.addTestStep("NumberOfRecords Selected", "NumberOfRecords Selected: " + actualNoOfRecords, STATUS.FAIL, DriverAction.takeSnapShot());
+        }
+    }
+    @Then("Verify number of records displayed changes")
+    public void verifyNumberOfRecordsDisplayedChanges() {
+        DriverAction.click(PolicyLocator.NoOfPolicyRecords);
+        String actualNoOfRecords = DriverAction.getElement(PolicyLocator.NoOfPolicyRecords).getText();
+        try {
+            if (actualNoOfRecords.equals("25"))
+                GemTestReporter.addTestStep("NumberOfRecords", "NumberOfRecords: " + actualNoOfRecords, STATUS.PASS, DriverAction.takeSnapShot());
+        } catch (Exception exception) {
+            GemTestReporter.addTestStep("NumberOfRecords", "NumberOfRecords: " + actualNoOfRecords, STATUS.FAIL, DriverAction.takeSnapShot());
+        }
+    }
+    @Then("Enter {string} in Search Box")
+    public void enterInSearchBox(String policyName) {
+        try {
+            DriverAction.typeText(PolicyLocator.searchPolicy, policyName);
+            GemTestReporter.addTestStep("Search PolicyName", "User searched the " + policyName + " in Search Box", STATUS.PASS, DriverAction.takeSnapShot());
+        } catch (Exception exception) {
+            GemTestReporter.addTestStep("Search PolicyName", "User not able to search the " + policyName + " in Search Box", STATUS.FAIL, DriverAction.takeSnapShot());
+        }
+    }
+    @Then("Verify the {string} on the dashboard")
+    public void verifyTheOnTheDashboard(String expectedMsg) {
+        DriverAction.waitSec(2);
+        String actualMsg = DriverAction.getElement(PolicyLocator.policyMessage).getText();
+        try {
+            if (actualMsg.equals(expectedMsg))
+                GemTestReporter.addTestStep("Error message", "Error message: " + actualMsg, STATUS.PASS, DriverAction.takeSnapShot());
+        } catch (Exception exception) {
+            GemTestReporter.addTestStep("Error message", "Error message: " + actualMsg, STATUS.FAIL, DriverAction.takeSnapShot());
+        }
+    }
+
 }
